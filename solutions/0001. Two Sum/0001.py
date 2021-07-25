6b8dab9765ddc4cff90684c93e881d2bb9bfdf12
Kimi’s Piano Journey <84212093+kimiwu2011@users.noreply.github.com>
2021-07-25 12:49:17 -0700

    Create 0001.py

---
+++ solutions/0001. Two Sum/0001.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    numToIndex = {}
+
+    for i, num in enumerate(nums):
+      if target - num in numToIndex:
+        return numToIndex[target - num], i
+      numToIndex[num] = i
