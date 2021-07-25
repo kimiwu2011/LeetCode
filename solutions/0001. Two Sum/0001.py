14e4f60accd4ce5361a2fea55f57c50880b59a41
Kimi’s Piano Journey <84212093+kimiwu2011@users.noreply.github.com>
2021-07-25 13:50:43 -0700

    Update 0001.py

--- solutions/0001. Two Sum/0001.py
+++ solutions/0001. Two Sum/0001.py
@@ -1,10 +1,26 @@
+#!/usr/bin/python3
 from typing import List
 
-class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    numToIndex = {}
+'''
+https://leetcode.com/problems/two-sum/
+'''
 
-    for i, num in enumerate(nums):
-      if target - num in numToIndex:
-        return numToIndex[target - num], i
-      numToIndex[num] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numToIndex = {}
+        for i, num in enumerate(nums):
+            if (target - num) in numToIndex:
+                return [numToIndex[target - num], i]
+            numToIndex[num] = i
+
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_func(self):
+        s = Solution()
+        self.assertEqual(s.twoSum([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(s.twoSum([3, 2, 4], 6), [1, 2])
+        self.assertEqual(s.twoSum([3, 3], 6), [0, 1])
+
+if __name__ == "__main__":
+    unittest.main()
