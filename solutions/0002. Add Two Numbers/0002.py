ce48e7ec9cfcda09b530101057dbb108bc1b3f9e
Kimi’s Piano Journey <84212093+kimiwu2011@users.noreply.github.com>
2021-07-25 22:56:23 -0700

    Update 0002.py

--- solutions/0002. Add Two Numbers/0002.py
+++ solutions/0002. Add Two Numbers/0002.py
@@ -31,9 +31,33 @@ class Solution:
 
 import unittest
 
+def array2listnode(arr: list):
+    ln = ListNode(0)
+    l = ln
+    for i in arr:
+        l.next = ListNode(i)
+        l = l.next
+    return ln.next
+
+def listnode2array(ln: ListNode):
+    arr = []
+    while ln:
+        arr.append(ln.val)
+        ln = ln.next
+    return arr
+
 class TestMain(unittest.TestCase):
     def test_func(self):
         s = Solution()
+        lns = (
+            ( [2, 4, 3], [5, 6, 4], [7, 0, 8]),
+            ( [0], [0], [0]),
+            ( [1, 8], [0], [1, 8]),
+            ( [5], [5], [0, 1]),
+        )
+        for l1, l2, l3 in lns:
+            l = s.addTwoNumbers(array2listnode(l1), array2listnode(l2))
+            self.assertEqual(listnode2array(l), l3)
 
 if __name__ == "__main__":
     unittest.main()
